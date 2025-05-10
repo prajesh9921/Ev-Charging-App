e6c445ef0bf447a293cd05cf75533de9a50c4a90
Shantanu Ingle <shantanu.ingle@toothsi.in>
2025-05-10 19:12:53 +0530

    custom marker component added

---
+++ components/CustomMarker.tsx
@@ -0,0 +1,36 @@
+import { ChargerDetails } from "@/constants/Chargers";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const CustomMarker = ({ charger, currentLocation }: { charger: ChargerDetails, currentLocation: ChargerDetails }) => {
+  const isSelected = currentLocation?.id === charger.id;
+  return (
+    <View style={isSelected ? styles.customMarkerSelected : styles.customMarker}>
+      <Text style={styles.markerText}>{charger.connector_types.length}</Text>
+    </View>
+  );
+};
+
+export default CustomMarker;
+
+const styles = StyleSheet.create({
+  customMarker: {
+    backgroundColor: "rgba(255,0,0,0.5)",
+    padding: 10,
+    borderRadius: 20,
+    borderColor: "#fff",
+    borderWidth: 1,
+  },
+  customMarkerSelected: {
+    backgroundColor: "rgba(0,255,0,0.5)",
+    padding: 10,
+    borderRadius: 20,
+    borderColor: "#fff",
+    borderWidth: 1,
+  },
+  markerText: {
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
